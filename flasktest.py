27d8021efc8d4b5e1f38274f14a3df6705ad7bc5
Sami Silvan <silvansami50@gmail.com>
2025-05-07 13:52:45 +0300

    working uploading and data conversion

--- flasktest.py
+++ flasktest.py
@@ -1,23 +1,32 @@
 from flask import Flask, render_template, url_for, request, redirect
 from werkzeug.utils import secure_filename
-import subprocess
+from synthetic import main
+import os
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+   return render_template("index.html")
 
-@app.route("/upload", methods=["POST"])
+@app.route("/upload", methods=["GET", "POST"])
 def upload_file():
-    file = request.files['file']
-    if file:
-        file.save(f'uploads/{secure_filename(file.filename)}')
-        subprocess.run(["python3", "synthetic.py"])
-        return redirect('/')
-    else:
-        return print("No file uploaded")
+    if request.method == "POST":
+        file = request.files['file']
+        if file:
+            upload_folder = 'SyntheticHealthData/uploads'
+            if not os.path.exists(upload_folder):
+                os.makedirs(upload_folder)
+
+            file_path = os.path.join(upload_folder, secure_filename(file.filename))
+            file.save(file_path)
+
+            main(file.filename)
+            return redirect('/')
+        else:
+            return "No file uploaded", 400
+    return "Invalid request method", 405
 
 
 app.run(debug=True, port=5001, host='0.0.0.0')
